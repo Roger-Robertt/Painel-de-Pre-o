1b338dbccaf9684aa78c16a10ef3570bccf762a7
Roger-Robertt <167802022+Roger-Robertt@users.noreply.github.com>
2026-05-01 20:27:41 -0300

    Delete Front-End/painel-de-combustivel/Back-End directory

--- Front-End/painel-de-combustivel/Back-End/Model/fornecedores.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type Fornecedor struct {
-	ID   int    `json:"id"`
-	Nome string `json:"nome"`
-	Cnpj string `json:"cnpj"`
-}
-
-type Fornecedores []Fornecedor
--- Front-End/painel-de-combustivel/Back-End/Model/produtoss.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type Produto struct {
-	ID   int    `json:"id"`
-	Nome string `json:"nome"`
-}
-
-type Produtos []Produto
