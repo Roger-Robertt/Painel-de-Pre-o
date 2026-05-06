8b397d3c9f856d10da0959ff1d33545963011bf5
Roger Robertt <rogerrobertsouzadossantos@gmail.com>
2026-05-06 20:18:25 -0300

    Alteração no projeto, tirando sujeiro no códigos

--- Back-End/Model/produtoss.go
+++ Back-End/Model/produtoss.go
@@ -2,7 +2,7 @@ package model
 
 type Produto struct {
     ID   uint   `gorm:"primaryKey" json:"id"`
-    Nome string `gorm:"not null" json:"nome"` // Ex: Diesel S10, Gasolina Comum
+    Nome string `gorm:"not null" json:"nome"`
 }
 
 type Produtos []Produto
